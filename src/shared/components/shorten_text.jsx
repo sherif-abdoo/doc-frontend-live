83f46186f555e5ef44c6d918d938f44fbb27dbbf
mars-890 <11410120235027@stud.cu.edu.eg>
2026-03-17 17:26:36 +0200

    Material String Size

--- src/shared/components/shorten_text.jsx
+++ src/shared/components/shorten_text.jsx
@@ -1,4 +1,4 @@
-const ShortenText = (text, limit = 35) => {
+const ShortenText = (text, limit = 40) => {
   if (typeof text !== "string") return ""; // safety check
   return text.length > limit ? text.slice(0, limit) + "..." : text;
 };
