29519a64d22eb63188be9f248f3b12cf1eeedb3b
Abdalla Sameh <154898979+aboda-7@users.noreply.github.com>
2026-07-02 21:08:01 +0300

    Editing Groups

--- src/pages/auth/sign_up_page.jsx
+++ src/pages/auth/sign_up_page.jsx
@@ -294,17 +294,29 @@ export default function SignUp() {
                         onChange={(e) => setGroup(e.target.value)}
                     >
                       <option value="">Select your group</option>
+                      <option value="Cmbs">Cmbs</option> 
                       <option value="Psbs">Psbs</option>
                       <option value="Nls">Nls</option>
                       <option value="Ice">Ice</option>
-                      <option value="Sbis">Sbis</option>
-                      <option value="Cmbs ,Mes ,Esc ,Fbs">Cmbs ,Mes ,Esc ,Fbs</option>
-                      <option value="A">A</option>
+                      <option value="Ghis">Ghis</option>
+                      <option value="A">A</option>  
                       <option value="B">B</option>
                       <option value="C">C</option>
                       <option value="D">D</option>
                       <option value="E">E</option>
                       <option value="F">F</option>
+                      <option value="G">G</option>
+                      <option value="H">H</option>
+                      <option value="I">I</option>
+                      <option value="J">J</option>
+                      <option value="K">K</option>
+                      <option value="L">L</option>
+                      <option value="M">M</option>
+                      <option value="N">N</option>
+                      <option value="O">O</option>
+                      <option value="P">P</option>
+                      <option value="Q">Q</option>
+                      <option value="R">R</option>
                     </select>
                   </div>
 
